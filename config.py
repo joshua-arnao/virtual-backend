3b34fba064864caa7bea255ee7a6c75556f1ab02
Joshua Arnao Canessa <joshua@Joshuas-MacBook.local>
2022-03-24 19:57:17 -0500

    limpieza de la rama

--- config.py
+++
@@ -1,6 +0,0 @@
-#Este archivo guardara todas las clases que se vayan a utilizar 
-from flask_sqlalchemy import SQLAlchemy
-from flask_marshmallow import Marshmallow
-
-validador = Marshmallow()
-conexion = SQLAlchemy()
